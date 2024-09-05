c9859c495821a6211e1cbadaea7745b6e2f5ed3d
Homerider <i@ateslikov.ru>
2024-09-06 00:18:13 +0300

    login/profile/me

---
+++ src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {authTokenInterceptor} from "./auth/auth.intercepto";
+
+export let appConfig: ApplicationConfig;
+appConfig = {
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authTokenInterceptor]))
+  ]
+};
---
+++ src/app/pages/search-page/search-page.component.ts
@@ -0,0 +1,26 @@
+import {AsyncPipe} from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {ProfileCardComponent} from '../../common-ui/profile-card/profile-card.component';
+import {Profile} from '../../data/interfaces/profile.interface';
+import {ProfileService} from '../../data/services/profile.service';
+
+
+@Component({
+  selector: 'app-search-page',
+  standalone: true,
+  imports: [
+    ProfileCardComponent,
+    AsyncPipe
+  ],
+  templateUrl: './search-page.component.html',
+  styleUrl: './search-page.component.scss'
+})
+export class SearchPageComponent {
+  profileService = inject(ProfileService)
+  profiles = this.profileService.filteredProfiles
+
+  constructor() {
+
+
+  }
+}
